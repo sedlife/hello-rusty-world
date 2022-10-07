a27e1411e5a2de1cf8e392b7f894a7c355fb8a05
ara-va <114382198+ara-va@users.noreply.github.com>
2022-10-07 18:31:31 +0530

    Added information about strings

--- main.rs
+++ main.rs
@@ -5,7 +5,9 @@ fn add2(x: i32, y: i32) -> i32 {
 fn main() {
     let x = 100;
     let y = 100;
-    let s1 = "Hello World";
+    // &str is a slice (&[u8]) that always points to a valid UTF-8 sequence.
+    let s1 : &str = "Hello World";
+    // String is heap allocated, growable and not null terminated.
     let s2 : String = "Hello World";
     println!("{}", add2(x, y));
     println!("Hello World");
